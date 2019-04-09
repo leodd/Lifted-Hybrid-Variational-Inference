2684dac4044252b3c9d0ffc5e78b57d71173b154
leodd <410528518@qq.com>
2019-04-09 11:32:37 -0500

    oneshot

--- OneShot.py
+++ OneShot.py
@@ -2,10 +2,18 @@ from Graph import *
 
 
 class OneShot:
-    def __init__(self, g):
+    def __init__(self, g, num_mixtures=5, num_quadrature_points=3):
         self.g = g
+        self.K = num_mixtures
+        self.quadrature_points = self.compute_quadrature_points(num_quadrature_points)
 
-    def gradient_w(self):
+    def compute_quadrature_points(self, n):
+        return 1
+
+    def gaussian_expectation(self):
+        pass
+
+    def gradient_tau(self):
         pass
 
     def gradient_mu_sig(self):
@@ -14,5 +22,5 @@ class OneShot:
     def gradient_category(self):
         pass
 
-    def run(self, iteration=100, quadrature_points=10):
+    def run(self, iteration=100):
         pass
