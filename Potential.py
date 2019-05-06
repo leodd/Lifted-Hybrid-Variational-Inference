use precision ('sig_inv') instead of covariance ('sig') in GaussianLogPotential, as precision mats are allowed to be non-PD and still define valid potential; fix bug in utils.broadcast_arrs_to_common_shape using the wrong backend based on arrs[0]

Potential.py
@@ -30,7 +30,7 @@ class GaussianPotential(Potential):
         return coef * pow(e, -0.5 * (x_mu * self.sig_inv * x_mu.T))
 
     def to_log_potential(self):
-        return GaussianLogPotential(self.mu, self.sig)
+        return GaussianLogPotential(self.mu, self.sig_inv)
 
         # def __eq__(self, other):
         #     return np.all(self.mu == other.mu) and np.all(self.sig == other.sig)  # self.w shouldn't make a difference
@@ -40,11 +40,9 @@ class GaussianPotential(Potential):
 
 
 class GaussianLogPotential:
-    def __init__(self, mu, sig):
+    def __init__(self, mu, sig_inv):
         self.mu = np.array(mu)
-        sig = np.array(sig)  # must be ndarray
-        self.sig = sig
-        self.sig_inv = np.linalg.inv(sig)
+        self.sig_inv = np.array(sig_inv)  # must be ndarray
 
     def __call__(self, args):
         """
@@ -62,12 +60,11 @@ class GaussianLogPotential:
             res = -0.5 * sig_inv[0, 0] * (args[0] - mu[0]) ** 2
         else:
             import utils
-            args = utils.broadcast_arrs_to_common_shape(args)
-
-            if isinstance(args[0], (tf.Variable, tf.Tensor)):
+            if any(isinstance(a, (tf.Variable, tf.Tensor)) for a in args):
                 backend = tf
             else:
                 backend = np
+            args = utils.broadcast_arrs_to_common_shape(args, backend=backend)
             v = backend.stack(args)  # n x ...
             args_ndim = len(v.shape) - 1
             mu = backend.reshape(mu, [n] + [1] * args_ndim)
@@ -83,10 +80,10 @@ class GaussianLogPotential:
         return res
 
         # def __eq__(self, other):
-        #     return np.all(self.mu == other.mu) and np.all(self.sig == other.sig)
+        #     return np.all(self.mu == other.mu) and np.all(self.sig_inv == other.sig_inv)
         #
         # def __hash__(self):
-        #     return hash((self.mu, self.sig))
+        #     return hash((self.mu, self.sig_inv))
 
 
 class LinearGaussianPotential(Potential):
@@ -113,8 +110,7 @@ class LinearGaussianPotential(Potential):
         mu = np.zeros(2)
         a = self.coeff
         sig_inv = np.array([[a ** 2, -a], [-a, 1.]]) / self.sig
-        sig = np.linalg.inv(sig_inv)
-        return GaussianLogPotential(mu, sig)
+        return GaussianLogPotential(mu, sig_inv)
 
 
 class X2Potential(Potential):
@@ -139,9 +135,9 @@ class X2Potential(Potential):
     def to_log_potential(self):
         # convert to equivalent Gaussian
         mu = np.zeros(1)
-        sig = np.zeros([1, 1])
-        sig[0] = self.sig / self.coeff
-        return GaussianLogPotential(mu, sig)
+        sig_inv = np.zeros([1, 1])
+        sig_inv[0, 0] = self.coeff / self.sig
+        return GaussianLogPotential(mu, sig_inv)
 
 
 class XYPotential(Potential):
@@ -167,8 +163,7 @@ class XYPotential(Potential):
         # convert to equivalent Gaussian
         mu = np.zeros(2)
         sig_inv = np.array([[0., 0.5], [0.5, 0.]]) * self.coeff / self.sig
-        sig = np.linalg.inv(sig_inv)
-        return GaussianLogPotential(mu, sig)
+        return GaussianLogPotential(mu, sig_inv)
 
 
 class ImageNodePotential(Potential):

osi/mixture_beliefs.py
@@ -350,7 +350,7 @@ def group_eval_log_potential_funs(factors_with_unique_log_potential_fun_types, u
 
         if log_potential_fun_type == GaussianLogPotential:
             like_axes = utils.broadcast_arrs_to_common_shape(utils.expand_dims_for_fun_grid(
-                like_axes))  # length n list, having common shape [c x ??? x V1 x V2 x ... Vn]
+                like_axes), backend=tf)  # length n list, having common shape [c x ??? x V1 x V2 x ... Vn]
             v = tf.stack(like_axes)  # n x c x ...
             mu = np.stack([f.mu for f in like_log_potential_funs], axis=-1)  # n x c
             mu = np.reshape(mu, [n, c] + [1] * (len(v.shape) - 2))  # n x c x ones

osi/utils.py
@@ -95,6 +95,7 @@ def get_partial_function(fun, n, partial_args_vals):
     return pfun
 
 
+# TODO: implement conditioning in terms of precision matrices instead
 def get_conditional_gaussian(mu, Sig, obs_args_vals):
     """
     Get the mean and covariance matrix of a conditional Gaussian distribution p(x_a | x_b), given the joint distribution
@@ -162,7 +163,7 @@ def condition_factors_on_evidence(factors, evidence):
                     log_pot = pot.to_log_potential()
                 else:  # may not be able to construct the conditional potential in closed-form
                     pot = copy(potential)
-                    pot.potential.get = get_partial_function(potential.get, n, partial_args_vals)
+                    pot.get = get_partial_function(potential.get, n, partial_args_vals)
                     assert factor.log_potential_fun is not None, \
                         "factor.log_potential_fun hasn't been set in the original graph, don't know how to condition"
                     log_pot = get_partial_function(factor.log_potential_fun, n, partial_args_vals)
@@ -312,7 +313,7 @@ def eval_fun_grid(fun, arrs, sep_args=False):
     return res
 
 
-def broadcast_arrs_to_common_shape(arrs):
+def broadcast_arrs_to_common_shape(arrs, backend):
     """
     Return a list of arrays that are all broadcasted to a common shape.
     e.g., if arrs have shapes [1, 3, 1] and [2, 1, 3], result will be a list of 2 arrays both with shape [2, 3, 3]
@@ -332,12 +333,13 @@ def broadcast_arrs_to_common_shape(arrs):
     if len(set(arrs_shapes)) == 1:  # all have the same shapes
         out = arrs
     else:
-        if isinstance(arrs[0], (tf.Variable, tf.Tensor)):
-            backend = tf
-        else:
-            backend = np
-        # TODO: allow arrs with different number of dimensions (prepend empty dimensions when needed)
-        assert len(set([len(s) for s in arrs_shapes])) == 1, 'arrs should have the same number of dimensions'
+        max_ndims = max([len(s) for s in arrs_shapes])
+        for i in range(len(arrs)):  # prepend empty dims if necessary
+            shape = arrs_shapes[i]
+            prepend_ndims = max_ndims - len(shape)
+            if prepend_ndims > 0:
+                arrs[i] = backend.reshape(arrs[i], (1,) * prepend_ndims + shape)
+        # assert len(set([len(s) for s in arrs_shapes])) == 1, 'arrs should have the same number of dimensions'
         arrs_shapes = np.array(arrs_shapes)
         # n, arrs_ndim = arrs_shapes.shape
         common_shape = tuple(np.max(arrs_shapes, axis=0))