a6ab52bb02ae89dc92480a894c15831c86e4470e
Yibo Yang <yiboyang@ymail.com>
2019-05-18 14:53:08 -0500

    add methods for getting marginals from BN parameterization of hybrid Gaussian MRF; add initial sketch of block Gibbs sampling

---
+++ gibbs/disc_mrf.py
@@ -0,0 +1,32 @@
+# Gibbs sampling in discrete MRF represented by factors
+# For now assuming there's no deterministic interaction (i.e., the distribution is positive) for simplicity
+# May need to do cythonize for speed
+
+import numpy as np
+from utils import softmax
+
+
+def gibbs_sample(lpot_tables, scopes, nbr_factor_ids, dstates, x, its=100):
+    """
+
+    :param lpot_tables:
+    :param scopes:
+    :param nbr_factor_ids:
+    :param dstates:
+    :param x:
+    :param its:
+    :return:
+    """
+    N = len(dstates)  # num nodes
+    for it in range(its):
+        for n in range(N):
+            d = dstates[n]
+            lprobs = np.zeros(d)  # floats
+            for f in nbr_factor_ids[n]:  # ids of neighboring factors
+                scope = scopes[f]  # list of node ids
+                lpot_table = lpot_tables[f]
+                idx = tuple(slice(None) if i == n else x[i] for i in scope)
+                lprobs += lpot_table[idx]
+            probs = softmax(lprobs)
+            x[n] = np.random.choice(d, p=probs)
+    return x
--- gibbs/hybrid_gaussian_mrf.py
+++ gibbs/hybrid_gaussian_mrf.py
@@ -2,28 +2,29 @@
 # discrete node values) are all exp quadratic
 # Also assume all discrete nodes have discrete states 0, 1, 2, ..., i.e., consecutive ints that can be used for indexing
 import numpy as np
-from Potential import QuadraticPotential, GaussianPotential, LinearGaussianPotential, X2Potential, XYPotential, \
-    LogQuadratic, LogTable, LogHybridQuadratic
-import math
+from Potential import QuadraticPotential, LogQuadratic, LogTable, LogHybridQuadratic
+from itertools import product
+from copy import deepcopy
 
 import utils
 
 
-def convert_to_bn(factors, rvs):
+def convert_to_bn(factors, rvs, Vd, Vc):
     """
     Brute-force convert a hybrid Gaussian MRF into equivalent Bayesian network, p(x_d, x_c) = p(x_d) p(x_c | x_d)
     :param factors: list of Graph.F
     :param rvs: list of Graph.RV
+    :param Vd: list of disc rvs
+    :param Vc: list of cont rvs
     :return:
     """
-    Vd = [rv for rv in rvs if rv.domain_type[0] == 'd']  # list of of discrete rvs
-    Vc = [rv for rv in rvs if rv.domain_type[0] == 'c']  # list of cont rvs
+    # Vd = [rv for rv in rvs if rv.domain_type[0] == 'd']  # list of of discrete rvs
+    # Vc = [rv for rv in rvs if rv.domain_type[0] == 'c']  # list of cont rvs
     Vd_idx = {n: i for (i, n) in enumerate(Vd)}
     Vc_idx = {n: i for (i, n) in enumerate(Vc)}
 
     Nd = len(Vd)
     Nc = len(Vc)
-    from itertools import product
     all_dstates = [rv.dstates for rv in Vd]  # [v1, v2, ..., v_Nd]
     all_disc_config = np.array(list(product(*all_dstates)))  # all joint configurations, \prod_i v_i by Nd
     disc_marginal_table = np.empty(all_dstates, dtype=float)  # [v1, v2, ..., v_Nd]
@@ -78,3 +79,138 @@ def convert_to_bn(factors, rvs):
     disc_marginal_table /= Z
 
     return disc_marginal_table, gaussian_means, gaussian_covs
+
+
+def get_crv_marg(disc_marginal_table, gaussian_means, gaussian_covs, Vc_idx, crv, flatten_params=True):
+    """
+    Find the parameters of the marginal distribution of a cont rv in a hybrid Gaussian MRF in Bayesian network
+    parameterization; result is a univariate GMM whose number of components equals number of joint disc states
+    :param disc_marginal_table: [v1, v2, ..., v_Nd]
+    :param gaussian_means: [v1, v2, ..., v_Nd, Nc]
+    :param gaussian_covs: [v1, v2, ..., v_Nd, Nc, Nc]
+    :param Vc_idx:
+    :param crv: rv object
+    :return:
+    """
+    idx = Vc_idx[crv]
+    weights = disc_marginal_table  # mixing weights
+    cond_gaussian_means = gaussian_means[..., idx]  # [v1, v2, ..., v_Nd]
+    cond_gaussian_vars = gaussian_covs[..., idx, idx]  # simply take diagonal; [v1, v2, ..., v_Nd]
+    if flatten_params:
+        weights, cond_gaussian_means, cond_gaussian_vars = \
+            tuple(map(np.ravel, [weights, cond_gaussian_means, cond_gaussian_vars]))
+
+    # might want to wrap things up in a GMM:
+    # https://scikit-learn.org/stable/modules/generated/sklearn.mixture.GaussianMixture.html
+    return weights, cond_gaussian_means, cond_gaussian_vars
+
+
+def get_drv_marg(disc_marginal_table, Vd_idx, drv):
+    """
+
+    :param disc_marginal_table:
+    :param drv:
+    :return:
+    """
+    idx = Vd_idx[drv]
+    Nd = len(disc_marginal_table.shape)
+    all_axes_except_idx = list(range(Nd))
+    all_axes_except_idx.remove(idx)
+    all_axes_except_idx = tuple(all_axes_except_idx)
+    return np.sum(disc_marginal_table, axis=all_axes_except_idx)
+
+
+import disc_mrf
+
+
+def block_gibbs_sample(factors, rvs, Vd, Vc, num_burnin, num_samples, init_x_d=None):
+    """
+
+    :param factors:
+    :param rvs:
+    :param Vd:
+    :param Vc:
+    :return:
+    """
+    int_type = int
+
+    Nd = len(Vd)
+    Nc = len(Vc)
+
+    all_dstates = [rv.dstates for rv in Vd]  # [v1, v2, ..., v_Nd]
+    if init_x_d is None:  # will start by sampling p(x_c | x_d)
+        init_x_d = np.array([np.random.randint(0, dstates) for dstates in all_dstates], dtype=int_type)
+    x_d = init_x_d
+
+    disc_samples = np.empty([num_samples, Nd], dtype=int_type)
+    cont_samples = np.empty([num_samples, Nc], dtype=float)
+
+    # pre-processing for sampling in cond disc MRF
+    cont_factors = []
+    disc_factors = []
+    disc_lpot_tables = []
+    disc_scopes = []
+    hybrid_factors = []  # strictly hybrid, i.e., contains both disc and cont
+    # strictly_hybrid_factor_ind = np.zeros(len(factors), dtype=bool)  # 'strictly hybrid' means contains disc AND cont
+    for j, factor in enumerate(factors):
+        lpot_fun = factor.log_potential_fun
+        if isinstance(lpot_fun, LogHybridQuadratic):
+            if len(np.shape(lpot_fun.c)) >= 1:
+                hybrid_factors.append(factor)
+                # strictly_hybrid_factor_ind[j] = 1
+            else:
+                cont_factors.append(factor)
+        elif isinstance(lpot_fun, LogTable):
+            disc_factors.append(factor)
+            disc_lpot_tables.append(lpot_fun.table)
+            disc_scopes.append(factor.disc_nb_idx)
+        else:
+            raise NotImplementedError
+
+    disc_nbr_factor_ids = [[] for _ in range(Nd)]
+    for j, scope in enumerate(disc_scopes):
+        for i in scope:
+            disc_nbr_factor_ids[i].append(j)
+
+    for it in range(num_burnin + num_samples):
+        # 1. sample p(x_c | x_d) in the conditional Gaussian MRF
+        # basically copied from convert_to_bn:
+        quadratic_factor_params = []  # each obtained from a (reduced) hybrid factor when given disc nb node values
+        quadratic_factor_scopes = []
+        # disc_pot_prod = 0  # table/discrete potential's contribution to \prod_c \psi_c (product of all potentials)
+        for factor in factors:  # TODO: speed up by precomputing joint_quadratic_params over cont_factors outside loop
+            lpot_fun = factor.log_potential_fun
+            if isinstance(lpot_fun, LogHybridQuadratic):
+                disc_vals_in_factor = tuple(x_d[i] for i in factor.disc_nb_idx)  # () is OK
+                quadratic_factor_params.append(lpot_fun.get_quadratic_params_given_x_d(disc_vals_in_factor))
+                quadratic_factor_scopes.append(factor.cont_nb_idx)
+
+        joint_quadratic_params = utils.get_joint_quadratic_params(quadratic_factor_params, quadratic_factor_scopes, Nc)
+        A, b, c = joint_quadratic_params
+        mu, Sig = utils.get_gaussian_mean_params_from_quadratic_params(A, b, mu_only=False)
+        x_c = np.random.multivariate_normal(mean=mu, cov=Sig)
+
+        # 2. sample p(x_d | x_c) in the conditional discrete MRF with table factors
+        # get contribution from strictly hybrid factors
+        cond_lpot_tables = []
+        cond_scopes = []
+        cond_disc_nbr_factor_ids = deepcopy(disc_nbr_factor_ids)  # TODO: ugly, somehow get rid of
+
+        for factor in hybrid_factors:
+            lpot_fun = factor.log_potential_fun
+            cont_vals_in_factor = x_c[factor.cont_nb_idx]
+            cond_lpot_tables.append(lpot_fun.get_table_params_given_x_c(cont_vals_in_factor))
+            cond_scopes.append(factor.disc_nb_idx)
+        for j, scope in enumerate(cond_scopes):
+            for i in scope:
+                cond_disc_nbr_factor_ids[i].append(j + len(disc_lpot_tables))
+        cond_lpot_tables = disc_lpot_tables + cond_lpot_tables
+        cond_scopes = disc_scopes + cond_scopes
+        x_d = disc_mrf.gibbs_sample(cond_lpot_tables, cond_scopes, cond_disc_nbr_factor_ids, all_dstates, x_d, its=100)
+
+        sample_it = it - num_burnin
+        if sample_it >= 0:
+            disc_samples[sample_it] = x_d
+            cont_samples[sample_it] = x_c
+
+    return disc_samples, cont_samples
