6823f696746758c0c0b10ee54de5d1a61eaa492e
leodd <410528518@qq.com>
2019-05-21 19:42:30 -0500

    fixed bugs

--- Demo/RGMKLDivergence.py
+++ Demo/RGMKLDivergence.py
@@ -9,7 +9,7 @@ import numpy as np
 import time
 from Demo.Data.RGM.Generator import generate_rel_graph, load_data
 from KLDivergence import kl_continuous
-# import osi.utils as utils
+import osi.utils as utils
 
 
 def norm_pdf(x, mu, sig):
@@ -32,19 +32,19 @@ map_err = list()
 
 for i in range(num_test):
     data = load_data('Data/RGM/' + str(i))
-    rel_g.data = dict()
+    rel_g.data = data
     g, rvs_table = rel_g.grounded_graph()
 
-    ans = GaBP(g)
-    ans.run(15, log_enable=False)
+    # ans = GaBP(g)
+    # ans.run(15, log_enable=False)
 
-    # obs_rvs = [v for v in g.rvs if v.value is not None]
-    # evidence = {rv: rv.value for rv in obs_rvs}
-    # cond_g = utils.get_conditional_mrf(g.factors_list, g.rvs, evidence)  # this will also condition log_potential_funs
-    # quadratic_params, rvs_idx = utils.get_quadratic_params_from_factor_graph(cond_g.factors, cond_g.rvs_list)
-    #
-    # mu, sig = utils.get_gaussian_mean_params_from_quadratic_params(A=quadratic_params[0], b=quadratic_params[1],
-    #                                                           mu_only=False)
+    obs_rvs = [v for v in g.rvs if v.value is not None]
+    evidence = {rv: rv.value for rv in obs_rvs}
+    cond_g = utils.get_conditional_mrf(g.factors_list, g.rvs, evidence)  # this will also condition log_potential_funs
+    quadratic_params, rvs_idx = utils.get_quadratic_params_from_factor_graph(cond_g.factors, cond_g.rvs_list)
+
+    mu, sig = utils.get_gaussian_mean_params_from_quadratic_params(A=quadratic_params[0], b=quadratic_params[1],
+                                                              mu_only=False)
 
     infer = LVI(g, num_mixtures=1, num_quadrature_points=3)
     infer.run(200, lr=0.1)
@@ -54,23 +54,23 @@ for i in range(num_test):
 
     for rv in g.rvs:
         if rv.value is None:
-            # rv_idx = rvs_idx[rv]
+            rv_idx = rvs_idx[rv]
             kl_temp.append(kl_continuous(
                 lambda x: infer.belief(x, rv),
-                lambda x: ans.belief(x, rv),
+                lambda x: norm_pdf(x, mu[rv_idx], sig[rv_idx, rv_idx]),
                 rv.domain.values[0],
                 rv.domain.values[1]
             ))
 
             map_err_temp.append(
-                abs(infer.map(rv) - ans.map(rv))
+                abs(infer.map(rv) - mu[rv_idx])
             )
 
     kl.extend(kl_temp)
     map_err.extend(map_err_temp)
 
     print('average KL:', np.average(kl_temp))
-    print('average MAP error:', np.max(map_err_temp), '±', np.std(map_err_temp))
+    print('average MAP error:', np.average(map_err_temp), '±', np.std(map_err_temp))
 
 print('average KL:', np.average(kl))
 print('average MAP error:', np.average(map_err), '±', np.std(map_err))
