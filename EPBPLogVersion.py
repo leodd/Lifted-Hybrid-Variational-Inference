2090ee4fc99cdab042dfdea76a65856b26872e2d
leodd <410528518@qq.com>
2019-05-25 11:13:42 -0500

    change epbp quad range

--- EPBPLogVersion.py
+++ EPBPLogVersion.py
@@ -320,7 +320,7 @@ class EPBP:
                     # )
                     z = quad(
                         lambda val: e ** self.belief_rv(val, rv, self.sample),
-                        rv.domain.values[0]-100, rv.domain.values[1]+100
+                        rv.domain.values[0]-10, rv.domain.values[1]+10
                     )[0]
                     shift = 0
                     self.cache[rv] = (z, shift)
