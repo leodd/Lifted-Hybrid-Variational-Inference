4650e6fdc645b29b58316e9182acdf84ce9d7670
leodd <410528518@qq.com>
2019-09-25 19:37:43 -0500

    add robot mapping dataset

--- Demo/HMLN/demo.py
+++ Demo/HMLN/demo.py
@@ -1,11 +1,24 @@
-from Demo.Data.HMLN.GeneratorRobotMapping import generate_rel_graph
+from Demo.Data.HMLN.GeneratorRobotMapping import generate_rel_graph, load_data
 from VarInference import VarInference as VI
 from LiftedVarInference import VarInference as LVI
 from C2FVarInference import VarInference as C2FVI
+from EPBPLogVersion import EPBP
 
 
 rel_g = generate_rel_graph()
 g, rvs_dict = rel_g.ground_graph()
+
+query = dict()
+for key, rv in rvs_dict.items():
+    if key[0] == 'PartOf' or key[0] == 'SegType':
+        query[key] = rv
+
+data = load_data('Demo/Data/HMLN/robot-map')
+for key, rv in rvs_dict.items():
+    if key not in data and key not in query and not rv.domain.continuous:
+        data[key] = 0  # closed world assumption
+
+g, rvs_dict = rel_g.add_evidence(data)
 
 infer = VI(g, num_mixtures=3, num_quadrature_points=3)
 infer.run(200, lr=0.2)
