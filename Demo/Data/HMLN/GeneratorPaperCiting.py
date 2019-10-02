feca2f9cc47f7fed1faa00e62deb44e7fdd7fe59
leodd <410528518@qq.com>
2019-10-02 13:36:07 -0500

    update PaperCiting Generator

--- Demo/Data/HMLN/GeneratorPaperCiting.py
+++ Demo/Data/HMLN/GeneratorPaperCiting.py
@@ -18,31 +18,27 @@ domain_bool = Domain((0, 1))
 domain_real = Domain((-15, 15), continuous=True, integral_points=linspace(-15, 15, 20))
 
 lv_paper = LV(Paper)
-lv_paper_2 = LV(Paper)
 lv_topic = LV(Topic)
 
-atom_paper_popularity = Atom(domain_real, logical_variables=(lv_paper,), name='popularity')
-atom_topic_popularity = Atom(domain_real, logical_variables=(lv_topic,), name='popularity')
-atom_paperIn = Atom(domain_bool, logical_variables=(lv_paper, lv_topic), name='paperIn')
-atom_paperIn_2 = Atom(domain_bool, logical_variables=(lv_paper_2, lv_topic), name='paperIn')
-atom_cites = Atom(domain_bool, logical_variables=(lv_paper, lv_paper_2), name='cites')
+atom_popularity = Atom(domain_real, logical_variables=(lv_paper,), name='Popularity')
+atom_paperIn = Atom(domain_bool, logical_variables=(lv_paper, lv_topic), name='PaperIn')
+atom_cites = Atom(domain_bool, logical_variables=(lv_paper, lv_paper), name='Cites')
 
 f1 = ParamF(
     MLNPotential(lambda x: imp_op(x[0] * x[1], x[2]), w=1),
-    nb=(atom_cites, atom_paperIn, atom_paperIn_2),
-    constrain=lambda sub: sub[lv_paper] != sub[lv_paper_2]
+    nb=['Cites(p1,p2)', 'PaperIn(p1,t)', 'PaperIn(p2,t)'],
+    constrain=lambda sub: sub['p1'] != sub['p2']
 )
 f2 = ParamF(
     MLNPotential(lambda x: x[0] * eq_op(x[1], x[2]), w=0.01),
-    nb=(atom_paperIn, atom_paper_popularity, atom_topic_popularity)
+    nb=['PaperIn(p,t)', 'Popularity(p)', 'Popularity(t)']
 )
 
 
 def generate_rel_graph():
-    rel_g = RelationalGraph()
-    rel_g.atoms = (atom_cites, atom_paperIn, atom_paper_popularity, atom_topic_popularity)
-    rel_g.param_factors = (f1, f2)
-    rel_g.init_nb()
+    atoms = (atom_cites, atom_paperIn, atom_popularity)
+    param_factors = (f1, f2)
+    rel_g = RelationalGraph(atoms, param_factors)
 
     return rel_g
 
@@ -52,23 +48,23 @@ def generate_data(f):
 
     X_ = np.random.choice(num_paper, int(num_paper * 0.2), replace=False)
     for x_ in X_:
-        data[str(('popularity', f'paper{x_}'))] = np.clip(np.random.normal(0, 3), -10, 10)
+        data[str(('Popularity', f'p{x_}'))] = np.clip(np.random.normal(0, 3), -10, 10)
 
     X_ = np.random.choice(num_topic, int(num_topic * 0.5), replace=False)
     for x_ in X_:
-        data[str(('popularity', f'topic{x_}'))] = np.clip(np.random.normal(0, 3), -10, 10)
+        data[str(('Popularity', f't{x_}'))] = np.clip(np.random.normal(0, 3), -10, 10)
 
     X_ = np.random.choice(num_paper, int(num_paper * 0.2), replace=False)
     for x_ in X_:
         Y_ = np.random.choice(num_topic, np.random.randint(3), replace=False)
         for y_ in Y_:
-            data[str(('paperIn', f'paper{x_}', f'topic{y_}'))] = int(np.random.choice([0, 1]))
+            data[str(('PaperIn', f'p{x_}', f't{y_}'))] = int(np.random.choice([0, 1]))
 
     X_ = np.random.choice(num_paper, int(num_paper * 1), replace=False)
     for x_ in X_:
         Y_ = np.random.choice(num_paper, int(num_paper * 1), replace=False)
         for y_ in Y_:
-            data[str(('cites', f'paper{x_}', f'paper{y_}'))] = int(np.random.choice([0, 0, 0, 1]))
+            data[str(('Cites', f'p{x_}', f'p{y_}'))] = int(np.random.choice([0, 0, 0, 1]))
 
     with open(f, 'w+') as file:
         file.write(json.dumps(data))
