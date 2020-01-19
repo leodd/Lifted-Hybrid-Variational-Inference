82a6dd497f74f05551d3c55b5c07366a83bb6b83
leodd <410528518@qq.com>
2020-01-19 01:59:35 -0600

    update plot

--- Demo/HMLN/HMLNTimeLog.py
+++ Demo/HMLN/HMLNTimeLog.py
@@ -1,6 +1,6 @@
 from utils import log_likelihood
-from Demo.Data.HMLN.GeneratorRobotMapping import generate_rel_graph, load_raw_data
-# from Demo.Data.HMLN.GeneratorPaperPopularity import generate_rel_graph, load_data
+# from Demo.Data.HMLN.GeneratorRobotMapping import generate_rel_graph, load_raw_data
+from Demo.Data.HMLN.GeneratorPaperPopularity import generate_rel_graph, load_data
 from VarInference import VarInference as VI
 from LiftedVarInference import VarInference as LVI
 from C2FVarInference import VarInference as C2FVI
@@ -12,15 +12,22 @@ import json
 rel_g = generate_rel_graph()
 _, rvs_dict = rel_g.ground_graph()
 
+# query = dict()
+# for key, rv in rvs_dict.items():
+#     if key[0] == 'SegType' or key[0] == 'PartOf' or key[0] == 'Length' or key[0] == 'Depth':
+#         query[key] = rv
+#
+# data = load_raw_data('Demo/Data/HMLN/robot-map')
+# for key, rv in rvs_dict.items():
+#     if key not in data and key not in query and not rv.domain.continuous:
+#         data[key] = 0  # closed world assumption
+
 query = dict()
 for key, rv in rvs_dict.items():
-    if key[0] == 'SegType' or key[0] == 'PartOf' or key[0] == 'Length' or key[0] == 'Depth':
+    if key[0] == 'PaperPopularity' or key[0] == 'TopicPopularity':
         query[key] = rv
 
-data = load_raw_data('Demo/Data/HMLN/robot-map')
-for key, rv in rvs_dict.items():
-    if key not in data and key not in query and not rv.domain.continuous:
-        data[key] = 0  # closed world assumption
+data = load_data('Demo/Data/HMLN/0')
 
 # data = dict()
 
@@ -30,7 +37,7 @@ print(len(g.factors))
 
 time_log = dict()
 
-with open('Demo/Data/HMLN/robot-mapping-time-log', 'r') as file:
+with open('Demo/Data/HMLN/paper-popularity-time-log', 'r') as file:
     s = file.read()
     time_log = json.loads(s)
 
@@ -46,13 +53,13 @@ with open('Demo/Data/HMLN/robot-mapping-time-log', 'r') as file:
 # infer.run(100, lr=0.2)
 # time_log['LVI'] = infer.time_log
 
-# infer = C2FVI(g, num_mixtures=2, num_quadrature_points=3)
-# infer.run(100, lr=0.2)
-# time_log['C2FVI'] = infer.time_log
+infer = C2FVI(g, num_mixtures=2, num_quadrature_points=3)
+infer.run(100, lr=0.2)
+time_log['C2FVI'] = infer.time_log
 
 
-# with open('Demo/Data/HMLN/robot-mapping-time-log', 'w+') as file:
-#     file.write(json.dumps(time_log))
+with open('Demo/Data/HMLN/paper-popularity-time-log', 'w+') as file:
+    file.write(json.dumps(time_log))
 
 
 import matplotlib.pyplot as plt
@@ -77,7 +84,7 @@ dash = {
     'C2FVI': [1, 1],
 }
 
-max_t = 500
+max_t = 80
 
 for name, t_log in time_log.items():
     x = list()
